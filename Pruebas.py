36a9bba7ffab5bb7e53c8b041278aca2c997b8c4
Leonardo Sicha <leopol742004@gmail.com>
2023-12-31 16:37:32 -0500

    Mostrando status corto

---
+++ Pruebas.py
@@ -0,0 +1,23 @@
+title = "se busca tripulante"
+message1 = "se ofrece puesto en la tripulación del capitán pyratilla para llevar a cabo labores de marinero." 
+message2 = "comida y bebida garantizada a lo largo de toda la aventura."
+contact = "para más información, ir al puerto y buscar al capitán pyratilla, ya famoso en este pueblo costero (evitar preguntar en el casino)."
+
+title_final=title.upper()
+
+message1=message1.capitalize()
+message1=message1.replace("pyratilla","Pyratilla")
+
+message2=message2.capitalize()
+message2=message2.replace("pyratilla","Pyratilla")
+
+contact=contact.capitalize()
+contact=contact.replace("pyratilla","Pyratilla")
+
+print(title_final+"\n"+message1+"\n"+message2+"\n"+contact)
+
+name=input("Introduzca su nombre: ")
+age=int(input("Introduzca su edad: "))
+hobby=input("¿Cuál es su hobby?: ")
+dream=input("¿Cuál es su sueño?: ")
+sds=input("hola")
