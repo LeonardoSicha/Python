d3e609dc465a2c03cb0ed8a0da91e7fea8d637d5
Leonardo Sicha <leopol742004@gmail.com>
2024-01-04 19:37:13 -0500

    Ejercicio 5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-Comp=7==float("7")
-print(Comp)
-=======
 s=input("Ingresa una frase: ")
 c=input("Ingresa el caracter a contar: ")
 if c in s:
     print("EL string tiene {} veces el caracter ingresado.".format(s.count(c)))
->>>>>>> 2b2e5f5a009928b9216d3dc60dddc5778b4ce799
