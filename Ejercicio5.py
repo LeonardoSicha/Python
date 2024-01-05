2b2e5f5a009928b9216d3dc60dddc5778b4ce799
Leonardo <leopol742004@gmail.com>
2024-01-04 19:27:57 -0500

    Agregando ejercicio 6

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,2 +1,4 @@
-print("Roskete marción")
-print("MArica")
+s=input("Ingresa una frase: ")
+c=input("Ingresa el caracter a contar: ")
+if c in s:
+    print("EL string tiene {} veces el caracter ingresado.".format(s.count(c)))
